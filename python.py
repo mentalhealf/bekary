c46c7964a92c34f939d1ba7c61c5932e98c0f3ed
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2024-01-21 20:05:23 +0000

    added animations

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
-print:"hello world":
+# this program prints hello, world!
+
+
+
+print('hello, world!')
+python_math (lol)
+HTML (0,0,0,1,1,1,2,2,2,3,3,3)
+CSS (0,1,1,1,1,2,2,2,3,3,3,4,4,4)
+jsc (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+python (idk bro)
+README_md (whats)
